05d36d16d90300451bf7cf8aefdaab356b84fb83
Alessandro Marega <ale_marega@hotmail.it>
2021-03-03 12:31:33 +0100

    fix sintassi

--- JSnack7/script.js
+++ JSnack7/script.js
@@ -1,5 +1,5 @@
 // creo variabile potenza di 2
-for (var i = 0; i <= 20; i++) {
+for (var i = 0; i <= 40; i++) {
   var potenza = Math.pow(2, i);
   if (potenza <= 1000) {
     console.log(potenza);
