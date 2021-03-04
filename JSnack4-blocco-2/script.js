056a0297f0864e2ea21cc533d864f92a2d019617
Alessandro Marega <ale_marega@hotmail.it>
2021-03-04 15:41:25 +0100

    aggiunta la somma e la stampa a JSnack4 blocco-2

--- JSnack4-blocco-2/script.js
+++ JSnack4-blocco-2/script.js
@@ -1,2 +1,13 @@
 // creo l'array di numeri interi
 var numeri = [1, 7, 8, 10, 121, 54, 56, 25, 27, 20, 34, 32];
+var somma = 0;
+
+// sommo tutti i numeri dispari dell'array
+for (var i = 0; i < numeri.length; i++) {
+  if (numeri[i] % 2 == 1) {
+    somma += numeri[i];
+  }
+}
+
+// stampo la somma
+console.log(somma);
