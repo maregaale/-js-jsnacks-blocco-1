f2a72340d6ec65d1a20915db42a6a49107240320
Alessandro Marega <ale_marega@hotmail.it>
2021-03-05 09:46:52 +0100

    corretto con indice dispari e non elemento a JSnack4-blocco-2

--- JSnack4-blocco-2/script.js
+++ JSnack4-blocco-2/script.js
@@ -2,9 +2,9 @@
 var numeri = [1, 7, 8, 10, 121, 54, 56, 25, 27, 20, 34, 32];
 var somma = 0;
 
-// sommo tutti i numeri dispari dell'array
+// sommo tutti i numeri con indice dispari dell'array
 for (var i = 0; i < numeri.length; i++) {
-  if (numeri[i] % 2 == 1) {
+  if (i % 2 == 1) {
     somma += numeri[i];
   }
 }
